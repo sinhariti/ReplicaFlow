3e4ff09981ca737ea0542c40b1dc9442da7ac2d9
Riti <112542247+sinhariti@users.noreply.github.com>
2025-12-19 12:12:28 +0530

    single node KV implemented

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ single node KV store - key value pairs are stores in a single server
 from fastapi import FastAPI
 app = FastAPI()
 store = {}
+
+@app.post("/put")
+def put(data: dict):
+    store[data['key']]=data['value']
+    return {"status":"success"}
+@app.get("/get/{key}")
+def get(key:str):
+    return {"value":store.get(key,"Key not found")}
+  
